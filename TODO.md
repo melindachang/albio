011f4b75749b6e28c1d3e43ede96a3d5c50730f0
Melinda Chang <melindachang.hy@gmail.com>
2022-09-08 23:57:40 -0700

    add: comments, todos, research commitment

--- TODO.md
+++ TODO.md
@@ -1,2 +1,13 @@
+# Features
 - [ ] Parse markup
 - [ ] Parse JS
+- [ ] Create templates
+- [ ] Extract logic into different files
+
+# Research
+- [ ] What `acorn` and `code_red` do
+- [ ] Everything about the AST we're setting up
+- [ ] How Svelte compiler avoids diffing and VDom
+- [ ] Differences between `parse5` and Svelte custom parser, see if it's worth implementing the latter
+    - [ ] Look into [docs](https://svelte.dev/docs#compile-time-svelte-parse) on the `svelte.parse()` method
+- [ ] How to get everything (e.g. `parseFragment()` method from `parse5`) to adhere to the return types we set with a template
--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,13 @@ import { parseFragment } from 'parse5';
 
 export const readFile = (path: string) => {
   let source: string = readFileSync(path, { encoding: 'utf8' });
-  const fragment = parseFragment(source);
+  const fragment = parseFragment(source); // Returns DocumentFragment (document object comprised of nodes) to be parsed based on contents of stringified HTML
 
   return extract(fragment);
 };
 
 export const extract = (fragment: any) => {
+  // Extracts nodes and logic from DocumentFragment. Returns an object consisting of all logic condensed in one string + all tags arranged in a list
   const tags: any[] = [];
   let code: string = '';
 
@@ -37,17 +38,18 @@ export const extract = (fragment: any) => {
       );
       if (!(index === -1)) {
         const path: string = node.attrs[index].value;
-        code += readFileSync(path);
+        code += readFileSync(path); // Parse JS files that <script> tags link to
       } else {
-        code += node.childNodes[0].value;
+        code += node.childNodes[0].value; // Parse simple JS content of <script> tags
       }
     } else {
-      tags.push(node);
+      tags.push(node); // Add non-script nodes to list of tags in the document
     }
   });
 
   return { code, tags };
 };
+// SCRIPT PARSER
 
 export const parseScript = (source: string) => {
   const AST = acorn.parse(source, {
@@ -68,5 +70,8 @@ export const walk = (AST: any) => {
 
   return { rest };
 };
+
+// TAG PARSER
+
 console.log(readFile('./test.html'));
 console.log(parseScript(readFile('./test.html').code));
